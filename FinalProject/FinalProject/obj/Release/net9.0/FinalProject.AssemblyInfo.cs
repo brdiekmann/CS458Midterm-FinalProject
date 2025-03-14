c6d689d77f2ee2918315fb586c1a47e878bb9576
Ethan Geibel <eth4366@gmail.com>
2025-03-13 20:36:18 -0500

    Added classes

--- FinalProject/FinalProject/obj/Release/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Release/net9.0/FinalProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8236173ea15c31bb135baafa961c8a6280877e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
